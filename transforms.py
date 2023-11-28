3b2d9068c445eda41e0dc49c0929bbdaaa6d10f6
Pramod Konara <Pramod.Konara@kognitiv.com>
2023-11-28 20:05:56 +0530

    Initial development

--- transforms.py
+++ transforms.py
@@ -1,7 +1,5 @@
 import apache_beam as beam
-from apache_beam.io import ReadFromText
-from apache_beam.io.gcp.bigquery import WriteToBigQuery
-from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions
+
 
 class AgeRangeTransform(beam.DoFn):
     def process(self, element):
